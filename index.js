fcaad79adca1b306edf22b7fb05b1c7d1b30e662
Boris <9309706@gmail.com>
2019-02-06 00:23:05 +0200

    v1.0.1 has been added

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const robot = require("robotjs");
+const ioHook = require("iohook");
 
-function randomInteger(min, max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1)
-    rand = Math.round(rand);
-    return rand;
+const CTRL = 29;
+const ALT = 56;
+const F7 = 65;
+
+class Clicker {
+    constructor() {
+        this.isEnabled = false;
+        this.timeoutId = null;
+
+        ioHook.start();
+
+        ioHook.registerShortcut([CTRL, ALT, F7], (keys) => {
+
+            this.isEnabled = !this.isEnabled;
+            if (!this.isEnabled) {
+                clearTimeout(this.timeoutId);
+                this.timeoutId = null;
+            } else {
+                this.doClick();
+            }
+        });
+    }
+
+    doClick() {
+        if (this.isEnabled) {
+            let pause = (this.randomInteger(1, 10) * 1000);
+            this.timeoutId = setTimeout(() => {
+                console.clear();
+                robot.mouseClick();
+                this.doClick();
+            }, pause);
+        }
+    }
+
+    randomInteger(min, max) {
+        let rand = min - 0.5 + Math.random() * (max - min + 1)
+        rand = Math.round(rand);
+        return rand;
+    }
 }
 
-setInterval(()=>{
-    console.clear();
-    robot.mouseClick();
-}, (randomInteger(1, 5) * 1000))
+new Clicker();
