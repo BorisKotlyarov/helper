f5d46b2fadb36f99c974251e5f21fb6e773b5a76
BorisKotlyarov <9309706@gmail.com>
2019-02-13 00:05:05 +0200

    hot fix

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const robot = require("robotjs");
 const ioHook = require("iohook");
 const settings = require('./settings');
+const pjson = require('./package.json');
 
 const CTRL = 29;
 //const ALT = 56;
 const F7 = 65;
 
 class Clicker {
+
     constructor() {
         this.isEnabled = false;
         this.timeoutId = null;
 
+        let argv = process.argv.slice();
+        argv.splice(0, 2);
+
+        switch (argv[0]) {
+
+            case new RegExp('^-(v||version)$', 'i').test(argv[0]) && argv[0]:
+                console.log('helper version', pjson.version);
+                break;
+
+            default:
+                this.init();
+        }
+
+    }
+
+    init() {
         ioHook.start();
 
         ioHook.registerShortcut([CTRL, F7], (keys) => {
@@ -38,7 +56,7 @@ class Clicker {
 
     randomMilliseconds(min, max) {
         let rand = min - 0.5 + Math.random() * (max - min + 1)
-        rand = Math.round(rand* 1000);
+        rand = Math.round(rand * 1000);
         return rand;
     }
 }
